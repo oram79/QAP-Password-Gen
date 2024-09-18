432bc9f8ad595897c437635d55f92124d4e89247
Logan Oram <logan.oram@keyin.com>
2024-09-18 20:27:01 -0230

    index.js started
    added user defaults and set password requirements

---
+++ index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const process = require("node:process");
+const args = process.argv.slice(2);
+//setting user defaults//
+const length = 8;
+let includeNumbers = false;
+let includeUppercase = false;
+let includeSymbols = false;
+
+//Generating the user password
+
+const genPassword = (length,includeNumbers,includeUppercase,includeSymbols)
+
+const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '123456789';
+const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const symbols = '!@#$%^&*()_+{}[]|;:,.<>?';
