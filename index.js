643b5ea8a16b927580d14bd05e65923ece2dbd83
Logan Oram <logan.oram@keyin.com>
2024-09-19 10:49:18 -0230

    mistake

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-#!/usr/bin/env node
-
-const process = require("node:process");
-const args = process.argv.slice(2);
-//setting user defaults//
-const length = 8;
-let includeNumbers = false;
-let includeUppercase = false;
 
 
-//Generating the user password
-
-const genPassword = (length,includeNumbers,includeUppercase,includeSymbols)
-
-const lowercase = 'abcdefghijklmnopqrstuvwxyz';
-const numbers = '123456789';
-const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
