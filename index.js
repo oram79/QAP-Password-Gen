a03dfdd3fb48ccd41833fbe4a430b4812e06c38d
Logan Oram <logan.oram@keyin.com>
2024-09-19 11:34:02 -0230

    CLI arguments made

--- index.js
+++ index.js
@@ -16,3 +16,39 @@ const generatePassword = (length, useNumbers, useCapitals) => {
     if (useNumbers) characters += numbers;
     if (useCapitals) characters += capitals
 
+    return Array.from({length}, () => characters[Math.floor(Math.random() * characters.length)]).join('');
+};
+
+//setting up the command line arguments
+for (let i = 0; i < args.length; i++) {
+    const arg = args[i];
+    switch (arg) {
+        
+        case "--help":
+        case "h":
+            displayHelp();
+            break;
+
+        case"--length":
+        case"-l":
+        length = parseInt(args[++i], 10);
+        if (isNaN(length) || length <= 0) {
+          console.error("Error: Length must be a positive number.");
+          process.exit(1);
+        }
+        break;
+
+        case "--numbers":
+        case "-n":
+            useNumbers = true;
+            break;
+        case "--capitals":
+        case "-c":
+            useCapitals = true;
+            break;
+
+        default:
+            console.error(`Error: Unknown option '${arg}'`);
+            displayHelp();
+    }
+}
