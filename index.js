3681d2e910b8100402c098f7bfccd4a51c0edd43
Logan Oram <logan.oram@keyin.com>
2024-09-19 11:21:30 -0230

    generatePassword function
    main password function made along with perameters

--- index.js
+++ index.js
@@ -4,4 +4,15 @@ const args = process.argv.slice(2);
 
 //Setting the default value for the user
 let length = 8
-let useNumbers = false, useCapitals = false, useSymbols = false;
+let useNumbers = false, useCapitals = false, useSymbols = false;let useNumbers = false, useCapitals = false;
+//Actually generating a password
+
+const generatePassword = (length, useNumbers, useCapitals) => {
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+    const numbers = "0123456789";
+    const capitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    //setting values
+    let characters  = letters;
+    if (useNumbers) characters += numbers;
+    if (useCapitals) characters += capitals
+
