c7ef8880415214d8f27868ef46d8c5eb9c8f517b
Logan Oram <logan.oram@keyin.com>
2024-09-19 10:58:20 -0230

    set default for user

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+#!/usr/bin/env node
 
+const args = process.argv.slice(2);
 
-
+//Setting the default value for the user
+let length = 8
+let useNumbers = false, useCapitals = false, useSymbols = false;
